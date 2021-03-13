2421c93dfb5b0ebba9a67334e6b52d7c0a92f4ef
Stefano Pigozzi <me@steffo.eu>
2021-03-13 17:13:42 +0100

    🐛 Properly implement claims

--- ryglfg/auth.py
+++ ryglfg/auth.py
@@ -23,7 +23,7 @@ log = logging.getLogger(__name__)
 class Auth0CustomClaims(p.BaseModel):
     iss: str
     sub: str
-    aud: t.List[str]
+    aud: t.Union[t.List[str], str]
     iat: int
     exp: int
     azp: str
